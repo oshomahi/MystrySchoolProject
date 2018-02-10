7019c1de4f057e1ded6143cc0bab545262f88737
mahesh.bk <mahesh.bk@ISSLT516>
2018-02-10 17:27:36 +0530

    Add project files.

---
+++ TheMysterySchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheMysterySchool.Startup))]
+namespace TheMysterySchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
